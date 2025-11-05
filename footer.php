45101df73652380d3649318eed6077318f12a6c7
Benita Almroth <benita.almroth@hotmail.com>
2025-11-05 20:45:40 +0100

    add:index, header, footer.php and started css

---
+++ footer.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" href="style.css">
+    <section>
+        <div class="footer-links">
+            <div class="shop">
+                <h3>Shop</h3>
+                <a href="">Find Store</a>
+                <a href="">Returns</a>
+                <a href="">Lifetime Guarantee</a>
+            </div>
+            <div class="company">
+                <h3>Company</h3>
+                <a href="">About us</a>
+                <a href="">Social media</a>
+                <a href="">careers</a>
+            </div>
+            <div class="support">
+                <h3>Support</h3>
+                <a href="">Help</a>
+                <a href="">FAQ</a>
+                <a href="">Accessability</a>
+            </div>
+        </div>
+        <div class="copy">
+            <p class="footer">  &copy; 2025 Kinforma. Alla rättigheter förbehållna</p>
+            <p>Privacy Policy</p>
+            <p>Terms of Service</p>
+        </div>
+    </section>
+</body>
+</html>
