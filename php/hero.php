237af16f6dbe74f3d790a855be6e1c5d663086a0
Benita Almroth <benita.almroth@hotmail.com>
2025-11-07 20:28:09 +0100

    finished hero mobile css and html, add: font weight var

--- php/hero.php
+++ php/hero.php
@@ -1,8 +1,13 @@
 <section class="hero">
-    <div class="rotating"> </div>
+
+    <div class="rotating"> 
+
+    </div>
+    
     <div class="hero-text">
       <h1>Huvudrubrik</h1>
       <h2>Underrubrik</h2>
       <button type="button">About Kinforma</button>
     </div>
+
   </section>
