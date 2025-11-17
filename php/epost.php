4f86e3bef1881e3078aa0211380026cb9aad6267
Benita Almroth <benita.almroth@hotmail.com>
2025-11-17 09:45:15 +0100

    added exclusive tag to epost, tweaked spacing

--- php/epost.php
+++ php/epost.php
@@ -1,16 +1,21 @@
   <section id="epostsub" class="epost">
     <div class="epost-container">
+      <div class="exclusive">
+        <span> Exclusive membership </span>
+      </div>
       <h2>Gå med i Kinforma Circle.</h2>
       <h3>Köp ett plagg – få tillgång till livslång omsorg, förköp och rösträtt i nästa släpp.</h3>
-      <ul>
-        <li>Påverka designen: rösta på färger, moduler och drops.</li>
-        <li>Möt andra: lokala träffar, arkivkvällar.</li>
-      </ul>
+      <div class="epost-ul">
+        <ul>
+          <li>Påverka designen: rösta på färger, moduler och drops.</li>
+          <li>Möt andra: lokala träffar, arkivkvällar.</li>
+        </ul>
+      </div>
       <div class="epost-input">
         <h3>Skriv in din e-post</h3>
         <form action="#epostsub" method="post">
         <label for= "email"></label>
-        <input type="email" name="email" placeholder="e-post">
+        <input type="email" name="email" placeholder="namn.efternamn@kinmail.com">
         <button type="submit">Gå med</button>
         <?php 
 
