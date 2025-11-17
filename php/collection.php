2c099f7a428de526493fed63f12474a52cf53adf
Robin Andersson <danrobin.andersson@outlook.com>
2025-11-17 10:03:14 +0100

    Added carousel and trending info

--- php/collection.php
+++ php/collection.php
@@ -1,45 +1,45 @@
-  <section class="collection"> 
-    <div class="collection-hero"> <!-- background img from dd/cd --> 
+  <section class="collection">
+    <div class="collection-hero"> <!-- background img from dd/cd -->
       <h2>Underrubrik Collection</h2>
       <h3> Kort Rubrik</h3>
+    </div>
+
+    <div class="products">
+      <div class="product">
+        <div class="product-img">
+          <!-- Image placeholder -->
+        </div>
+        <h3>Outerwear</h3>
       </div>
-      
-      <div class="products">
-        <div class="product">
-          <div class="product-img">
-            <!-- Image placeholder -->
-          </div>
-          <h3>Kortrubrik</h3>
+      <div class="product">
+        <div class="product-img">
+          <!-- Image placeholder -->
         </div>
-        <div class="product">
-          <div class="product-img">
-            <!-- Image placeholder -->
-          </div>
-          <h3>Kortrubrik</h3>
-        </div>
-        <div class="product">
-          <div class="product-img">
-            <!-- Image placeholder -->
-          </div>
-          <h3>Kortrubrik</h3>
-        </div>
-        <div class="product">
-          <div class="product-img">
-            <!-- Image placeholder -->
-          </div>
-          <h3>Kortrubrik</h3>
-        </div>
-        <div class="product">
-          <div class="product-img">
-            <!-- Image placeholder -->
-          </div>
-          <h3>Kortrubrik</h3>
-        </div>
-        <div class="product">
-          <div class="product-img">
-            <!-- Image placeholder -->
-          </div>
-          <h3>Kortrubrik</h3>
-        </div>
+        <h3>Clothing</h3>
       </div>
-    </section>
+      <div class="product">
+        <div class="product-img">
+          <!-- Image placeholder -->
+        </div>
+        <h3>Merch</h3>
+      </div>
+      <div class="product">
+        <div class="product-img">
+          <!-- Image placeholder -->
+        </div>
+        <h3>Accessories</h3>
+      </div>
+      <div class="product">
+        <div class="product-img">
+          <!-- Image placeholder -->
+        </div>
+        <h3>Members only</h3>
+      </div>
+      <div class="product">
+        <div class="product-img">
+          <!-- Image placeholder -->
+        </div>
+        <h3>Beauty</h3>
+      </div>
+    </div>
+  </section>
