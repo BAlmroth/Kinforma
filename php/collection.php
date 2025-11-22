abeed1505356c42406e48bc09558166d76d7831f
Benita Almroth <benita.almroth@hotmail.com>
2025-11-22 01:01:25 +0100

    added hover element to collection, fixed spacing

--- php/collection.php
+++ php/collection.php
@@ -5,36 +5,42 @@
       <div class="product">
         <div class="product-img">
           <img src="/images/Ytterkläder.png" alt="Ytterkläder">
+          <img class="img-hover" src="/images/brownjacketgirl.png" alt="Kläder">
         </div>
         <h3>YTTERKLÄDER</h3>
       </div>
       <div class="product">
         <div class="product-img">
-          <img src="/images/Kläder.png" alt="Kläder">
+          <img class="img-default" src="/images/Kläder.png" alt="Kläder">
+          <img class="img-hover" src="/images/brownjacketgirl.png" alt="Kläder">
         </div>
         <h3>KLÄDER</h3>
       </div>
       <div class="product">
         <div class="product-img">
           <img src="/images/Merch.png" alt="Merch">
+          <img class="img-hover" src="/images/brownjacketgirl.png" alt="Kläder">
         </div>
         <h3>MERCH</h3>
       </div>
       <div class="product">
         <div class="product-img">
           <img src="/images/Accessoarer.png" alt="Accessoarer">
+          <img class="img-hover" src="/images/brownjacketgirl.png" alt="Kläder">
         </div>
         <h3>ACCESSOARER</h3>
       </div>
       <div class="product">
         <div class="product-img">
           <img src="/images/För medlemmar.png" alt="För medlemmar">
+          <img class="img-hover" src="/images/brownjacketgirl.png" alt="Kläder">
         </div>
         <h3>FÖR MEDLEMMAR</h3>
       </div>
       <div class="product">
         <div class="product-img">
           <img src="/images/Skönhet.png" alt="Skönhet">
+          <img class="img-hover" src="/images/brownjacketgirl.png" alt="Kläder">
         </div>
         <h3>SKÖNHET</h3>
       </div>
