a2d54947e77c0a891db1e9b6502d0a8041134354
Benita Almroth <benita.almroth@hotmail.com>
2025-11-24 18:35:10 +0100

    changed collection images

--- php/collection.php
+++ php/collection.php
@@ -4,45 +4,45 @@
     <div class="products">
       <div class="product">
         <div class="product-img">
-          <img src="images/outerwear.png" alt="Ytterkläder">
+          <img class="img-default left-position" src="images/outerwear.png" alt="Ytterkläder">
           <img class="img-hover" src="images/brownjacketgirl.png" alt="Kläder">
         </div>
-        <h3>YTTERKLÄDER</h3>
+        <h5>YTTERKLÄDER</h5>
       </div>
       <div class="product">
         <div class="product-img">
-          <img class="img-default" src="images/clothes.png" alt="Kläder">
+          <img class="img-default zoom-clothes" src="images/clothes.png" alt="Kläder">
           <img class="img-hover" src="images/brownjacketgirl.png" alt="Kläder">
         </div>
-        <h3>KLÄDER</h3>
+        <h5>KLÄDER</h5>
       </div>
       <div class="product">
         <div class="product-img">
-          <img src="images/Merch.png" alt="Merch">
+          <img class="img-default" src="images/Merch.png" alt="Merch">
           <img class="img-hover" src="images/brownjacketgirl.png" alt="Kläder">
         </div>
-        <h3>MERCH</h3>
+        <h5>MERCH</h5>
       </div>
       <div class="product">
         <div class="product-img">
-          <img src="images/Accessoarer.png" alt="Accessoarer">
+          <img class="img-default" src="images/Accessoarer.png" alt="Accessoarer">
           <img class="img-hover" src="images/brownjacketgirl.png" alt="Kläder">
         </div>
-        <h3>ACCESSOARER</h3>
+        <h5>ACCESSOARER</h5>
       </div>
       <div class="product">
         <div class="product-img">
-          <img src="images/formembers.png" alt="För medlemmar">
+          <img class="img-default" src="images/formembers.png" alt="För medlemmar">
           <img class="img-hover" src="images/brownjacketgirl.png" alt="Kläder">
         </div>
-        <h3>FÖR MEDLEMMAR</h3>
+        <h5>MEDLEMMAR</h5>
       </div>
       <div class="product">
         <div class="product-img">
-          <img src="images/beauty.png" alt="Skönhet">
+          <img class="img-default" src="images/beauty.png" alt="Skönhet">
           <img class="img-hover" src="images/brownjacketgirl.png" alt="Kläder">
         </div>
-        <h3>SKÖNHET</h3>
+        <h5>SKÖNHET</h5>
       </div>
     </div>
   </section>
