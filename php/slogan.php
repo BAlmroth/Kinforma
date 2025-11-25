2f96168dd3a520787e37b42e3f11fb09df16f155
Benita Almroth <benita.almroth@hotmail.com>
2025-11-25 20:19:52 +0100

    tweaks on values and hero

--- php/slogan.php
+++ php/slogan.php
@@ -1,6 +1,8 @@
 <section class="slogan">
-    <video autoplay muted loop id="kinformaVideo">
-        <source src="/images/Kinforma.mp4" type="video/mp4">
-    </video>
-    <img src="images/orange-logo.svg" alt="logo">
+    <div class="slogan-background">
+        <video autoplay muted loop>
+            <source src="/images/Kinforma.mp4" type="video/mp4">
+        </video>
+        <img src="images/orange-logo.svg" alt="logo">
+    </div>
 </section>
